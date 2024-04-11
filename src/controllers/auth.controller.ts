d7a9c570fce26c494f01999645442419a2531150
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-04-10 22:48:55 -0500

    feat: :sparkles: Password encryption functionality completed

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from 'express';
 import { UserModel } from '../models/users.model';
+import { bcryptAdapter } from '../config/bcrypt.adapter';
 
 export const Register = async(req: Request, res: Response) => {
   try {
     const user = await UserModel.create(req.body);
+
+    user.password = bcryptAdapter.hash( req.body.password );
+    await user.save();
 
     return res.json({
       user
