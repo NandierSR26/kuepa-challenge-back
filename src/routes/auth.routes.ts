dcb8cc83c9c8fb8a79f37b78e93d82d3d660b9e2
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-04-11 01:06:53 -0500

    feat: :sparkles: Added endpoint for auth validation

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
-import { Register, login } from '../controllers/auth.controller';
+import { Register, login, validateAuth } from '../controllers/auth.controller';
 import { validateUsername } from '../middlewares/verifyUser';
 
 const router = Router();
+
+router.get('/validate', validateAuth);
 
 router.post('/login', [], login);
 
