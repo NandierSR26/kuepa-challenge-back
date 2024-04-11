e29f8c0bf322cccd895048acdf54bfb5d7776474
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-04-10 20:51:35 -0500

    feat: :sparkles: Initial configuration of user model completed

---
+++ src/models/users.model.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from 'mongoose';
+
+const UsersSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required']
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: true
+  },
+  photo: {
+    type: String
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required']
+  },
+  rol: {
+    type: [String],
+    default: ['STUDENT'],
+    enum: ['STUDENT', 'MODERATOR']
+  }
+});
+
+export const UserModel = mongoose.model('User', UsersSchema);
