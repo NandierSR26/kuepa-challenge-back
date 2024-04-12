6c4d7d89e2b08ce20b7219def193544b4b683644
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-04-12 10:24:00 -0500

    fix: :bug: Git ignore updated

--- dist/routes/chat.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const chat_controller_1 = require("../controllers/chat.controller");
-const validateJWT_1 = require("../middlewares/validateJWT");
-const router = (0, express_1.Router)();
-router.get('/from/:id', [validateJWT_1.validateJWT], chat_controller_1.getIndividualChat);
-router.get('/group', [validateJWT_1.validateJWT], chat_controller_1.getGroupChat);
-exports.default = router;
--- dist/types/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.destinationTypes = void 0;
-exports.destinationTypes = {
-    USER: 'USER',
-    GROUP: 'GROUP'
-};
